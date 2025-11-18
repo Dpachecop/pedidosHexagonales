b49eb8eceab261363a01ba1b4209c81fb31c089c
Daniel pacheco <dpachper@gmail.com>
2025-11-18 10:46:52 -0500

    primer commit

---
+++ src/main/java/com/daniel/pedidosHexagonales/PedidosHexagonalesApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.pedidosHexagonales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosHexagonalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosHexagonalesApplication.class, args);
+	}
+
+}
