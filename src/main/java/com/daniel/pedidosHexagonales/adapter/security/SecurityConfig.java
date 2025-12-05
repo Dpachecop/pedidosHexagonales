a4c2b1b453bab389038666b7b4382f813677ca3c
Daniel pacheco <dpachper@gmail.com>
2025-12-04 23:23:42 -0500

    agregar config para front

--- src/main/java/com/daniel/pedidosHexagonales/adapter/security/SecurityConfig.java
+++ src/main/java/com/daniel/pedidosHexagonales/adapter/security/SecurityConfig.java
@@ -65,4 +65,15 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowedOrigins(java.util.List.of("*")); // Permite a TODO el mundo (Para pruebas)
+        configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(java.util.List.of("*"));
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
